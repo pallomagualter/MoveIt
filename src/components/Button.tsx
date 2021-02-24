8da65f00f873a3fa19697e1c340cf16a7238e6cb
Palloma Gualter <pallomagualter@gmail.com>
2021-02-24 16:38:26 -0300

    props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,25 @@
-//pode ser type também mais iremos utilizar interface
+import { useState } from 'react';
 
+//pode ser type também mais iremos utilizar interface
 interface ButtonProps {
     color: string;
     children: string;
 }
 
 export function Button(props: ButtonProps) {
+    const [counter, setCounter] = useState(1);
+
+    function increment() {
+        setCounter(counter + 1);
+    }
+
     return (
-        <button type="button" style={{ backgroundColor: props.color }} >
-            {props.children}
+        <button 
+            type="button" 
+            style={{ backgroundColor: props.color }}
+            onClick={increment}
+            >
+                {props.children} <strong>{counter}</strong>
         </button>
     )
 }
