614f168c0a0c851ffe286ede26c0528c3f0efb1b
Palloma Gualter <pallomagualter@gmail.com>
2021-02-24 16:06:31 -0300

    props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 export function Button(props: ButtonProps) {
     return (
-        <button type="button"  >
+        <button type="button" style={{ backgroundColor: props.color }} >
             {props.color}
         </button>
     )
