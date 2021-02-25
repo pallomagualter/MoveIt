d6382c461e407e70f7198df0451182c2422187ae
Palloma Gualter <pallomagualter@gmail.com>
2021-02-25 16:00:00 -0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { Button } from './components/Button';
-
 import './styles/global.css';
 
 function App() {
   return (
     <div>
       <h1> Hi, Palloma!</h1>
-      <Button color= "red" >Cancelar</Button>
-      <Button color= "green" >Salvar</Button>
-      <Button color= "blue" >Editar</Button>
     </div> 
   );
 }
