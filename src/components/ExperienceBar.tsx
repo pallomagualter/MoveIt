8a358aa8da672d6ce2c21e0afd8f5767ab711ed9
Palloma Gualter <pallomagualter@gmail.com>
2021-02-25 16:41:07 -0300

    level experience

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -2,7 +2,9 @@ export function ExperienceBar() {
     return (
         <header className="experience-bar">
             <span>0 xp</span>
-            <div />
+            <div>
+                <div style={{ width: '60%' }} />
+            </div>
             <span>600 xp</span>
         </header>
     );
