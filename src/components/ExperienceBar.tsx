0ce6cee2a18ff8bf901aa15ba10e82c48d539557
Palloma Gualter <pallomagualter@gmail.com>
2021-02-26 16:42:01 -0300

    experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -3,7 +3,11 @@ export function ExperienceBar() {
         <header className="experience-bar">
             <span>0 xp</span>
             <div>
-                <div style={{ width: '60%' }} />
+                <div  style={{ width: '50%' }} />
+
+                <span className="current-experience" style={{ left: '50%' }}>
+                    300 xp
+                </span>
             </div>
             <span>600 xp</span>
         </header>
