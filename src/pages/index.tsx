d3445d1ad5dd76e11382ef6dfe064ec27172ad8a
Palloma Gualter <pallomagualter@gmail.com>
2021-03-04 09:21:38 -0300

    migration for next

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { ExperienceBar } from '../components/ExperienceBar';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <head>
+        
+      </head>
+
+    <ExperienceBar />
+  </div> 
+  )
+}
