69f79eeac286d2d4f845a4ed52207ba6b553d5ce
Palloma Gualter <pallomagualter@gmail.com>
2021-03-04 15:23:40 -0300

    Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,9 @@
+export function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/pallomagualter.png" alt="Palloma Gualter" />
+
+            
+        </div>
+    );
+}
