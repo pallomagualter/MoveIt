04ef578b5b5b9b6160f8c3737c86c58bd5c303ff
Palloma Gualter <pallomagualter@gmail.com>
2021-03-05 10:47:02 -0300

    countdown css

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,8 +2,8 @@ import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
     return(
-        <div>
-            
+        <div className={styles.countdownContainer}>
+
         </div>
     );
 }
