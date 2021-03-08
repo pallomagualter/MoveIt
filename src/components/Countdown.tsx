1a577ac64d107b1357c7ce11bcf3223651b67804
Palloma Gualter <pallomagualter@gmail.com>
2021-03-08 10:31:15 -0300

    boa prática

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
     const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
     const minutes = Math.floor(time / 60); //para arredondar o valor para baixo
     const seconds = time % 60;
@@ -12,16 +12,16 @@ export function Countdown() {
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown() {
-        setActive(true);
+        setIsActive(true);
     }
 
     useEffect(() => {
-        if (active && time > 0) {
+        if (isActive && time > 0) {
             setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
         }
-    }, [active, time]) //executado sempre que o active e o time mudarem
+    }, [isActive, time]) //executado sempre que o active e o time mudarem
 
     return (
         <div>
