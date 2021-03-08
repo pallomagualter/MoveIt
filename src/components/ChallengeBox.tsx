f76d06baa5dd7359b3797a9aa1b8f232be31357b
Palloma Gualter <pallomagualter@gmail.com>
2021-03-08 16:32:06 -0300

    challenge box

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,5 +1,11 @@
+import styles from '../styles/components/ChallengeBox.module.css';
+
 export function ChallengeBox() {
     return (
-        <div></div>
+        <div className={styles.challengeBoxContainer}>
+          <div className={styles.challengeNotActive}>
+            <strong>Inicie um ciclo para receber desafios a serem completados</strong>
+          </div>
+        </div>
     )
 }
