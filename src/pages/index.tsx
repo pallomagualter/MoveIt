5fa1f11771e3326abc95c5e66ca3a4e1be275f7f
Palloma Gualter <pallomagualter@gmail.com>
2021-03-09 15:38:16 -0300

    challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
+import Head from 'next/head';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { ChallengeBox } from '../components/ChallengeBox';
 import { Profile } from '../components/Profile';
-
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
